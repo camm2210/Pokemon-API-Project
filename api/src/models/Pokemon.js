30bd39e5a142fd96576112352062fcc6721db3d6
camm2210 <camilodiaza37@gmail.com>
2023-05-10 20:55:30 -0500

    routes and DB models created

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -5,11 +5,44 @@ module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("pokemon", {
     id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    hp: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    attack: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    defense: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    speed: {
+      type: DataTypes.STRING,
+    },
+    height: {
+      type: DataTypes.STRING,
+    },
+    weight: {
+      type: DataTypes.STRING,
+    },
+    created: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
     },
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,30 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
 const router = Router();
+
+router.get("/pokemons", (req, res) => {
+  res.send("Todos los pokemons");
+});
+
+router.get("/pokemons/name?=pokemon", (req, res) => {
+  res.send("pokemons por nombre en query");
+});
+
+router.get("/pokemons/:idPokemons", (req, res) => {
+  res.send("pokemons por id");
+});
+
+router.post("/pokemons", (req, res) => {
+  res.send("crear un pokemon");
+});
+
+router.get("/types", (req, res) => {
+  res.send("obtener tipos de pokemon");
+});
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
 
 module.exports = router;
