d5b01614f46ff02bf04db4ac8797e5a2a6c4868e
camm2210 <camilodiaza37@gmail.com>
2023-05-19 05:33:18 -0500

    Front navBar creation

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import { Landing, Home, Detail, Form } from "./views";
-
+import NavBar from "./components/NavBar/Navbar";
 import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <Route exact path="/" render={() => <Landing />} />
       <Route path="/home" render={() => <Home />} />
       <Route exact path="/detail" render={() => <Detail />} />
